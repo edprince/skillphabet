43211caea60a93e2847fe50c0233a56058707458
ed <edward_prince@hotmail.com>
2022-12-04 23:36:25 +0100

    pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,9 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=IM+Fell+Double+Pica:ital@1&family=Nanum+Pen+Script&display=block" rel="stylesheet"></link>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/manifest/icon512x512.png"></link>
+                <meta name="theme-color" content="#a5b4fb" />
             </Head>
             <body>
                 <Main />
