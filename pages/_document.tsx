1f53c058f80cde2af9f01e821a2b24e3bb312f4f
ed <edward_prince@hotmail.com>
2022-12-05 09:27:32 +0100

    added opengraph data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,10 @@ export default function Document() {
                 <link rel="manifest" href="/manifest/manifest.json" />
                 <link rel="apple-touch-icon" href="/manifest/icon512x512.png"></link>
                 <meta name="theme-color" content="#a5b4fb" />
+                <meta property="og:title" content="Skillphabet" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.skillphabet.com" />
+                <meta property="og:image" content="/opengraph.png" />
             </Head>
             <body>
                 <Main />
